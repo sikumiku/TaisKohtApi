a7ec2f82219497621ad1498c7c12cd1ca8884b7c
evelinj <evelin.jogi@itcollege.ee>
2018-04-21 13:46:49 +0300

    test

--- TaisKohtApi/Controllers/ValuesController.cs
+++ TaisKohtApi/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace TaisKohtApi.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "test";
         }
 
         // POST api/values
