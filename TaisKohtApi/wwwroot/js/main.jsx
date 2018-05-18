214c541b93a31cba69c9d52ce1884d5210b9ce2c
manrico <marko.nou@gmail.com>
2018-05-18 17:23:52 +0300

    Add react and javascript main

---
+++ TaisKohtApi/wwwroot/js/main.jsx
@@ -0,0 +1,13 @@
+﻿var ContentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="contentBox">
+              Main
+      </div>
+        );
+    }
+});
+ReactDOM.render(
+    <ContentBox />,
+    document.getElementById('content')
+);
