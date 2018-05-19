e1dc900fb20bab2e1ebc4d759559f32c8585c202
manrico <marko.nou@gmail.com>
2018-05-19 13:29:16 +0300

    Add styles + style minifyer plugin to webpack

--- TaisKohtApi/webpack.config.js
+++ TaisKohtApi/webpack.config.js
@@ -1,8 +1,28 @@
 ﻿const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('allstyles.css');
+
 module.exports = {
     entry: './wwwroot/source/app.js',
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js'
+    },
+    plugins: [
+        extractCSS,
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default']
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/, use: extractCSS.extract(['css-loader?minimize'])
+            }
+        ]
     }
 };
--- TaisKohtApi/wwwroot/Source/app.js
+++ TaisKohtApi/wwwroot/Source/app.js
@@ -1,4 +1,5 @@
-﻿$ = require('jquery');
-require('./lib');
+﻿require('./lib');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/site.css';
 document.getElementById("fillthis").innerHTML = getText();
 $('#fillthiswithjquery').html('Filled by Jquery!');
