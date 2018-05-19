72091167e70d7e5460ee320e63224317d8f12e6c
manrico <marko.nou@gmail.com>
2018-05-20 00:55:34 +0300

    Add password field

--- TaisKohtApi/wwwroot/Source/components/pages/auth/login.js
+++ TaisKohtApi/wwwroot/Source/components/pages/auth/login.js
@@ -22,8 +22,12 @@ class LoginForm extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    Username:
+                     <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Password:
+                     <input type="password" value={this.state.value} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
