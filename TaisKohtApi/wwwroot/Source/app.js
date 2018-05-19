575e4ca6321bc38a78ee30f2a9f1a565a91f6a92
manrico <marko.nou@gmail.com>
2018-05-19 14:59:38 +0300

    Remove old components

--- TaisKohtApi/wwwroot/Source/app.js
+++ TaisKohtApi/wwwroot/Source/app.js
@@ -1,9 +1,7 @@
-﻿//require('./lib');
-import 'bootstrap/dist/css/bootstrap.min.css';
+﻿import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/site.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './reactcomponent';
 import FetchData from './fetchdata';
 
 ReactDOM.render(
