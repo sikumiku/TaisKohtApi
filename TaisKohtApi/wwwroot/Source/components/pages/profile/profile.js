2c6631f7af8ff64efa12f3059011ffe783e16488
Martin Kask <kask.martin@gmail.com>
2018-05-27 10:27:45 -0700

    Create Profile dummy view

---
+++ TaisKohtApi/wwwroot/Source/components/pages/profile/profile.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                Profile
+            </div>
+    );
+}
+}
+
+export default MainPage;
