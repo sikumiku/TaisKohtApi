496f2788814bc1e9efa4e85f12a3c8ac229150d5
manrico <marko.nou@gmail.com>
2018-05-27 16:08:26 +0300

    Refactor dishcontroller as standalone component

--- TaisKohtApi/wwwroot/Source/components/pages/menu/RestaurantMenu.js
+++ TaisKohtApi/wwwroot/Source/components/pages/menu/RestaurantMenu.js
@@ -1,36 +1,53 @@
 ﻿import * as React from 'react';
 import 'es6-promise';
 import 'isomorphic-fetch';
-import MenuItem from './MenuItem';
+import Dish from './Dish';
+
 export default class RestaurantMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { menu: [], loading: true };
-        this.restaurantId = this.props.match.params.restaurantId;
-        this.refreshData();
+        this.state = {
+            menuContent: null,
+            spinner: 'EMPTY',
+            menu: this.props.menu
+        };
     }
     render() {
-        let contents = this.state.loading ? <p><em>Loading...</em></p>
-            : RestaurantMenu.renderMenuList(this.state.menu);
+        let contents;
+        if (this.state.spinner === 'EMPTY') {
+            contents = "";
+        } else if (this.state.spinner === 'LOADING') {
+            contents = "spinner";
+        } else if (this.state.spinner === 'LOADED' && this.state.menuContent != null) {
+            contents = RestaurantMenu.renderExpandedInfo(this.state.menuContent);
+        }
 
-        return <div>
-            <h1>Restaurant Menu</h1>
-            <button onClick={() => { this.refreshData() }}>Refresh</button>
-            <p>This component fetches menu data from the server.</p>
-            {contents}
-        </div>;
+        return (
+            <div>
+                <b>
+                    <a href="#" onClick={() => { this.getMenuContent() }}>{this.state.menu.name}</a></b>
+                {contents}
+            </div>
+
+        );
     }
-    refreshData() {
-        fetch('/api/v1/Restaurants/' + this.restaurantId)
+
+    getMenuContent() {
+        this.state.spinner = 'LOADING';
+        //this.state.menu.menuId
+        fetch('/api/v1/Menus/5')
             .then(response => response.json())
             .then(data => {
-                this.setState({ menu: data, loading: false });
+                this.setState({ menuContent: data, spinner: 'LOADED' });
             });
     }
-    static renderMenuList(menu) {
-        return <div className='menuItem'>
-            {menu.map(dish =>
-                <MenuItem dish={dish} />
+
+    static renderExpandedInfo(menuContent) {
+        console.log(menuContent);
+        return <div>
+            <b>Dishes :</b>
+            {menuContent.dishes.map(dish =>
+                <Dish dish={dish} />
             )}
         </div>;
     }
