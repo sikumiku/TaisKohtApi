416dcbd92054ca32a161660d2a8b29f4b0fe8ea1
manrico <marko.nou@gmail.com>
2018-05-27 14:58:35 +0300

    Add dynamic loading into content

--- TaisKohtApi/wwwroot/Source/components/pages/restaurants/RestaurantListItem.js
+++ TaisKohtApi/wwwroot/Source/components/pages/restaurants/RestaurantListItem.js
@@ -2,24 +2,53 @@
 import { Link , Route} from 'react-router-dom';
 import RestaurantMenu from '../menu/RestaurantMenu';
 
-class RestaurantListItem extends Component {
+
+export default class RestaurantListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurantInfo: null,
+            spinner: 'EMPTY',
+            restaurant: this.props.restaurant
+        };
+    }
     render() {
-        const restaurant = this.props.restaurant;
+            let contents;
+            if (this.state.spinner === 'EMPTY') {
+                contents = "";
+            } else if (this.state.spinner === 'LOADING') {
+                contents = "spinner";
+            } else if (this.state.spinner === 'LOADED' && this.state.restaurantInfo != null) {
+                contents = RestaurantListItem.renderExpandedInfo(this.state.restaurantInfo);
+            }
+       
         return (
             <div>
-                {restaurant.name}
-                {restaurant.url}
-                {restaurant.contactNumber}
-                {restaurant.email}
+                <a href="#" onClick={() => { this.getRestaurantInfo() }}>{this.state.restaurant.name}</a>
+                {this.state.restaurant.url}
+                {this.state.restaurant.contactNumber}
+                {this.state.restaurant.email}
                 <div>
-                    <Link to={'/restaurant/' + restaurant.restaurantId}>
-                        Menu
-                    </Link>
+                    {contents}  
                 </div>
             </div>
 
         );
     }
-}
 
-export default RestaurantListItem;
+    getRestaurantInfo() {
+        this.state.spinner = 'LOADING';
+        fetch('/api/v1/Restaurants/' + this.state.restaurant.restaurantId)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ restaurantInfo: data, spinner: 'LOADED' });
+            });
+    }
+
+    static renderExpandedInfo(restaurantInfo) {
+        console.log(restaurantInfo);
+        return <div>
+           restorani extended info.
+        </div>;
+    }
+}
