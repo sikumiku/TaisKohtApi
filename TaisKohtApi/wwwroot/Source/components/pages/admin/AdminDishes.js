63aea68f5d172abf0530d74539dd317d458b6787
manrico <marko.nou@gmail.com>
2018-06-08 20:40:59 +0300

    Initial commit fir dish controller

--- TaisKohtApi/wwwroot/Source/components/pages/admin/AdminDishes.js
+++ TaisKohtApi/wwwroot/Source/components/pages/admin/AdminDishes.js
@@ -1,148 +1,192 @@
 ﻿import * as React from 'react';
 import 'es6-promise';
 import 'isomorphic-fetch';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import AuthService from '../Auth/AuthService';
+import RestaurantListItem from './RestaurantListItem';
+import MenuListItem from './MenuListItem';
 const Auth = new AuthService();
+import './admin.css';
+import axios from 'axios';
+
 
 export default class AdminDishes extends React.Component {
     constructor() {
         super();
+
         this.state = {
             dishes: [],
+            userMenus: [],
             loading: true,
             name: null,
-            url: null,
-            addressFirstLine: null,
-            locality: null,
-            postcode: null,
-            region: null,
-            country: null
+            activeFrom: null,
+            activeTo: null,
+            RepetitionInterval: null,
+            restaurantId: null,
         };
 
         this.handleChange = this.handleChange.bind(this);
-        this.postDish = this.postDish.bind(this);
+        this.postMenu = this.postDish.bind(this);
 
-        //this.getUserRestaurants();
+        this.getUserMenus();
+        this.getUserDishes();
     }
     render() {
-
-        let contents = AdminDishes.renderUserDishesList(this.state.dishes);
+        let contents = this.state.loading ? <p><em>Loading...</em></p>
+            : AdminMenus.renderUserDishList(this.state.dishes);
+        let menuOptionValues =
+            this.state.userMenus.map(restaurant =>
+                <option value={restaurant.restaurantId}>{restaurant.name}</option>
+            );
 
         return <div>
             <div className="page-header">Profile</div>
-            Your restaurants: 
+            Your menus:
             {contents}
 
-            <div className="card">
-                <h1>Add new dish</h1>
-                <form onSubmit={this.postDish}>
-                    <input
-                        className="form-item"
-                        placeholder="Restaurant name"
-                        name="name"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Url goes here"
-                        name="url"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Email goes here"
-                        name="email"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Address first line goes here"
-                        name="addressFirstLine"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Locality goes here"
-                        name="locality"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Postcode goes here"
-                        name="postcode"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Region goes here"
-                        name="region"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-item"
-                        placeholder="Country goes here"
-                        name="country"
-                        type="text"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        className="form-submit"
-                        value="SUBMIT"
-                        type="submit"
-                    />
+            <div className="EditForm">
+                <div className="page-header">Add new Menu</div>
+                <form onSubmit={this.postMenu}>
+                    <FormGroup controlId="name" bsSize="small">
+                        <ControlLabel>Menu name</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="activeFrom" bsSize="small">
+                        <ControlLabel>Active from</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="date"
+                            value={this.state.activeFrom}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="activeTo" bsSize="small">
+                        <ControlLabel>Active to</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="date"
+
+                            value={this.state.activeTo}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="RepetitionInterval" bsSize="small">
+                        <ControlLabel>Repetition Interval</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="number"
+                            value={this.state.RepetitionInterval}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="restaurantId" bsSize="small">
+                        <ControlLabel>Restaurant</ControlLabel>
+                        <FormControl componentClass="select"
+                            placeholder="select"
+                            value={this.state.restaurantId}
+                            onChange={this.handleChange}
+                        >
+                            {restaurantOptionValues}
+                        </FormControl>
+                    </FormGroup>
+                    <Button block bsSize="large" type="submit">Add Menu</Button>
                 </form>
             </div>
         </div>;
     }
 
-    postDish(e) {
+    postMenu(e) {
         e.preventDefault();
-        var postData = {
-            method: 'POST',
+        let postData = JSON.stringify({
+            'name': this.state.name,
+            'activeFrom': this.state.activeFrom,
+            'activeTo': this.state.activeTo,
+            'RepetitionInterval': this.state.repetitionInterval,
+            'restaurantId': this.state.restaurantId,
+        });
+
+        let headers = {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + Auth.getToken(),
-            },
-            body: JSON.stringify({
-                'name': this.state.name,
-                'url': this.state.url,
-                'address': {
-                    "addressFirstLine": this.state.addressFirstLine,
-                    "locality": this.state.locality,
-                    "postcode": this.state.postcode,
-                    "region": this.state.region,
-                    "country": this.state.country
-                }
-            })
+            }
         };
 
-        fetch('api/v1/Restaurants', postData)
-            .then(response => response.json())
-            .then(data => {
+        axios.post('/api/v1/Menus', postData, headers)
+            .then(response => {
                 console.log(data);
-        });
+                this.getUserMenus();
+            })
+            .catch(err => {
+                console.log(err.response.data);
+                alert(err.response.data);
+            });
+    }
+
+    getUserMenus() {
+        let headers = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + Auth.getToken(),
+            }
+        };
+
+        axios.get('/api/v1/menus/owner', headers)
+            .then(response => {
+                console.log(response);
+                this.setState({ menus: response.data, loading: false });
+            }).catch(err => {
+                console.log(err.response.data);
+                alert(err.response.data);
+            });
+    }
+
+    getUserRestaurants() {
+        let headers = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + Auth.getToken(),
+            }
+        };
+
+        axios.get('/api/v1/restaurants/owner', headers)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    userRestaurants: response.data,
+                    loading: false,
+                    restaurantId: response.data[0].restaurantId
+                });
+
+            }).catch(err => {
+                console.log(err.response.data);
+                alert(err.response.data);
+            });
     }
 
     handleChange(e) {
         console.log(e);
         this.setState(
             {
-                [e.target.name]: e.target.value
+                [e.target.id]: e.target.value
             }
         )
     }
 
-    static renderUserDishList(restaurants) {
-        return <div className='restaurantList'>
-            your restaurant list here with edit and delete buttons.
-            </div>
+
+    static renderUserDishist(menus) {
+        return <div className='menusList' >
+            {menus.map(menu =>
+                <MenuListItem menu={menu} />
+            )}
+        </div>
     }
 }
